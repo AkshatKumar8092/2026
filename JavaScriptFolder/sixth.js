07f11669d31eae88822c94f8b1642595272c444e
Akshat Kumar <kr.akshat2002@gmail.com>
2026-01-17 15:06:05 +0530

    Video 6 Start

---
+++ JavaScriptFolder/sixth.js
@@ -0,0 +1,13 @@
+// 3 core behing web dev
+
+/*
+
+HTML: Structure, what to keep and where to click
+css: style, make the HTML beautiful
+js: logic, what should happen and where to happen
+
+
+
+
+
+*/
